1fc68b8d2d9fec5c2c0bb438ed7418cafeff0e03
jpchiodini <jpchiodini@comast.net>
2016-04-25 23:12:34 -0400

    Finished data cleaning portion. Now saves back to Json

--- SentimentAnalysis/DataClean.py
+++ SentimentAnalysis/DataClean.py
@@ -7,13 +7,14 @@ from RAKE import Rake
 import operator
 from nltk.corpus import stopwords
 import string
+import json as json
 
 def readJson(filename):
     """
     reads a json file and returns a clean pandas data frame
     """
     import pandas as pd
-    df = pd.read_json(filename)  
+    df = pd.read_json(filename)     
     
     #allows us join all the subarrays in keywords
     def unlist(element):
@@ -25,16 +26,18 @@ def readJson(filename):
     df['date'] = df['date'].apply(lambda x: x[:10])  
     df['date'] = pd.to_datetime(df['date'])   
     
-    ##
-
     for index, row in df.iterrows():
         if row['keywords'] == "":
-            df.set_value(index,'keywords', giveKeyword(row['body']))           
+            df.set_value(index,'keywords', giveKeyword(row['body']))                       
 
-    df = df.sort(columns='date')
-    print df
+    df = df.sort(columns = 'date')
+    df['body'] = df['body'].apply(cleanText) ##clean the data a final time
     
+    # use ix to reorder
+    df = df.ix[:, ['date', 'keywords', 'body']]    
     return df
+
+
 
 def cleanText(text):
     """
@@ -55,6 +58,36 @@ def cleanText(text):
 
     return clean
 
+#def to_json(df,filename):
+#    d = [ 
+#        dict([
+#            (colname, row[i]) 
+#            for i,colname in enumerate(df.columns)
+#        ])
+#        for row in df.values
+#    ]
+#    return json.dump(d, open(filename + '.json', 'w'),default=json_serial)
+
+def to_json(df,filename):
+    d = [ 
+        dict([
+            (colname, row[i]) 
+            for i,colname in enumerate(df.columns)
+        ])
+        for row in df.values
+    ]
+    return json.dump(d, open(filename + '.json', 'w'),default=json_serial)
+
+from datetime import datetime
+
+def json_serial(obj):
+    """JSON serializer for objects not serializable by default json code"""
+
+    if isinstance(obj, datetime):
+        serial = obj.isoformat()
+        return serial
+    raise TypeError("Type not serializable")
+
 def giveKeyword(text):
     from bs4 import BeautifulSoup
     text = BeautifulSoup(text,"lxml").get_text()   
@@ -72,11 +105,8 @@ def giveKeyword(text):
     output = keywords[0][0] + "," + keywords[1][0]
 
     return output  #the highest ranked one...
-
-
 currentDf = readJson('NonKeywordExample.json')
+to_json(currentDf, 'testexample')
 
-#now we need to find a way to format and save as json or csv...... 
-
-
+#now we need to find a way to format and save as json or csv......
 nargb = 1
